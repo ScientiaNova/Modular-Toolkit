d3cd03552eec3a7651261399f0390375e0e3b978
EmosewaPixel <awesomeggaming@gmail.com>
2019-07-12 15:46:09 +0200

    I forgot about the part tooltips

--- src/main/java/com/NovumScientiaTeam/modulartoolkit/proxy/ClientProxy.java
+++ src/main/java/com/NovumScientiaTeam/modulartoolkit/proxy/ClientProxy.java
@@ -6,11 +6,13 @@ import com.EmosewaPixel.pixellib.materialsystem.types.ObjectType;
 import com.EmosewaPixel.pixellib.proxy.IModProxy;
 import com.NovumScientiaTeam.modulartoolkit.ModularToolkit;
 import com.NovumScientiaTeam.modulartoolkit.ObjTypeRegistry;
-import com.NovumScientiaTeam.modulartoolkit.tools.util.ToolUtils;
+import com.NovumScientiaTeam.modulartoolkit.abilities.Abilities;
+import com.NovumScientiaTeam.modulartoolkit.abilities.AbstractAbility;
 import com.NovumScientiaTeam.modulartoolkit.partTypes.Extra;
 import com.NovumScientiaTeam.modulartoolkit.partTypes.Handle;
 import com.NovumScientiaTeam.modulartoolkit.partTypes.Head;
 import com.NovumScientiaTeam.modulartoolkit.tools.ToolRegistry;
+import com.NovumScientiaTeam.modulartoolkit.tools.util.ToolUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.util.text.ITextComponent;
@@ -35,12 +37,12 @@ public class ClientProxy implements IModProxy {
 
     @Override
     public void enque(InterModEnqueueEvent interModEnqueueEvent) {
-        ToolRegistry.TOOLS.forEach(t->
-        Minecraft.getInstance().getItemColors().register((stack, index) -> {
-            if (ToolUtils.getToolMaterial(stack, index) != null)
-                return ToolUtils.getToolMaterial(stack, index).getColor();
-            return -1;
-        }, t));
+        ToolRegistry.TOOLS.forEach(t ->
+                Minecraft.getInstance().getItemColors().register((stack, index) -> {
+                    if (ToolUtils.getToolMaterial(stack, index) != null)
+                        return ToolUtils.getToolMaterial(stack, index).getColor();
+                    return -1;
+                }, t));
     }
 
     @Override
@@ -56,8 +58,12 @@ public class ClientProxy implements IModProxy {
             Material mat = ((IMaterialItem) item).getMaterial();
             if (mat.getItemTier() != null) {
                 DecimalFormat format = new DecimalFormat("#.##");
+                AbstractAbility ability;
                 if (obj.hasTag(ObjTypeRegistry.HEAD)) {
                     Head head = new Head(null);
+                    ability = Abilities.getFor(mat, head);
+                    if (ability != null)
+                        e.getToolTip().add(ability.getTranslationKey(e.getItemStack()));
                     e.getToolTip().add(underline(new TranslationTextComponent("tool_part_type.head")));
                     e.getToolTip().add(new TranslationTextComponent("tool.stat.attack_damage", format.format(mat.getItemTier().getAttackDamage() + 1)));
                     if (!obj.hasTag(ObjTypeRegistry.WEAPON_PART)) {
@@ -69,14 +75,21 @@ public class ClientProxy implements IModProxy {
                 }
                 if (obj.hasTag(ObjTypeRegistry.HANDLE)) {
                     Handle handle = new Handle();
+                    ability = Abilities.getFor(mat, handle);
+                    if (ability != null)
+                        e.getToolTip().add(ability.getTranslationKey(e.getItemStack()));
                     e.getToolTip().add(underline(new TranslationTextComponent("tool_part_type.handle")));
                     e.getToolTip().add(new TranslationTextComponent("tool.stat.durability", Integer.toString(handle.getExtraDurability(mat))));
                     e.getToolTip().add(new TranslationTextComponent("tool.stat.durability_multiplier", format.format(handle.getDurabilityModifier(mat))));
                 }
                 if (obj.hasTag(ObjTypeRegistry.EXTRA)) {
+                    Extra extra = new Extra();
+                    ability = Abilities.getFor(mat, extra);
+                    if (ability != null)
+                        e.getToolTip().add(ability.getTranslationKey(e.getItemStack()));
                     e.getToolTip().add(underline(new TranslationTextComponent("tool_part_type.extra")));
-                    e.getToolTip().add(new TranslationTextComponent("tool.stat.durability", Integer.toString(new Extra().getExtraDurability(mat))));
-                    e.getToolTip().add(new TranslationTextComponent("tool.stat.level_cap_multiplier", format.format(new Extra().getLevelCapMultiplier(mat))));
+                    e.getToolTip().add(new TranslationTextComponent("tool.stat.durability", Integer.toString(extra.getExtraDurability(mat))));
+                    e.getToolTip().add(new TranslationTextComponent("tool.stat.level_cap_multiplier", format.format(extra.getLevelCapMultiplier(mat))));
                 }
             }
         }
